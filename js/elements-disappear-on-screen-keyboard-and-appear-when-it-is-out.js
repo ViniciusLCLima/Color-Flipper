e78f9dfc902544edd1adefd65887782480319af7
ViniciusLeitedoCarmo <vinicius.docarmo1998@gmail.com>
2022-07-25 17:17:47 -0300

    Small fix on function name.

--- js/elements-disappear-on-screen-keyboard-and-appear-when-it-is-out.js
+++ js/elements-disappear-on-screen-keyboard-and-appear-when-it-is-out.js
@@ -4,19 +4,19 @@ const presentationSticker = document.querySelector('.presentation-sticker');
 const inputs = document.querySelectorAll('.main-btn-div input');
 const classToAddOrDelOnKeyboardAppearance = 'on-keyboard-appearance'
 
-const addListenerToAllElementsOfArrayOnEvent=(array, event, listener)=>{
+const addListenerToAllElementsOfArray=(array, event, listener)=>{
     for (let i=0; i<(array.length); i++){
     array[i].addEventListener(event,listener)
     }
 }
 
-addListenerToAllElementsOfArrayOnEvent(inputs, 'focus',()=>{
+addListenerToAllElementsOfArray(inputs, 'focus',()=>{
     mainMsg.classList.add(classToAddOrDelOnKeyboardAppearance);
     myFooterPresentation.classList.add(classToAddOrDelOnKeyboardAppearance);
     presentationSticker.classList.add(classToAddOrDelOnKeyboardAppearance);
     })
 
-addListenerToAllElementsOfArrayOnEvent(inputs, 'focusout',()=>{
+addListenerToAllElementsOfArray(inputs, 'focusout',()=>{
         mainMsg.classList.remove(classToAddOrDelOnKeyboardAppearance);
         myFooterPresentation.classList.remove(classToAddOrDelOnKeyboardAppearance);
         presentationSticker.classList.remove(classToAddOrDelOnKeyboardAppearance);
